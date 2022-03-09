154c44069e5c7c83d8448f2ba52149843c5d9830
kwonghyun <momky4@naver.com>
2022-03-09 22:03:31 +0900

    pollss

--- polls/models.py
+++ polls/models.py
@@ -5,8 +5,10 @@ from django.utils import timezone
 
 # Create your models here.
 class Question(models.Model):
+    id = models.AutoField(primary_key=True)
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+   
 
     def __str__(self):
         return self.question_text
@@ -17,6 +19,7 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
+    id = models.AutoField(primary_key=True)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
